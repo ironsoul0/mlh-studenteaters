37c65a1a557b59ac2838e3cd7f1d135dcad793d1
ironsoul0 <timka2609@gmail.com>
2019-12-02 16:02:14 +0600

    Basic setup for Content

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import Button from '../ScrollBar/Button/Button';
 import './Content.css';
 
-function Content() {
+import { connect } from 'react-redux';
+
+function Content(props) {
+  let renderContent = null;
+
+  if (props.chosen === -1) {
+    renderContent = (
+      <div className="Content__intro">
+        <h1 className="Content__greetings">
+          Hi,
+        </h1>
+        <p className="Content__text">
+          Let's make it fair enough!
+        </p>
+        <p className="Content__subtext">
+          Read others reviews to decide!
+        </p>
+        <p className="Content__subtext">
+          Leave your own reviews to help NU community grow!
+        </p>
+      </div>
+    );
+  } else {
+    renderContent = (
+      <p>some bullshit</p>
+    );
+  }
+
   return (
     <div class="Content">
-      <Button name="Free Flow" about="Eating" active={false} />
+     {renderContent}
     </div>
   );
 }
 
-export default Content;
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+    chosen: state.chosen
+  }
+}
+
+export default connect(mapStateToProps)(Content);
