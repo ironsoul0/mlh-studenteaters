7a988eee8970a18317b9c666c8e049fb6161bccf
Akezh <rakishev.akezhan@mail.ru>
2019-12-02 14:40:56 +0600

    feature/EAT-1

---
+++ src/components/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Button.css';
+
+function Button() {
+  return (
+    <div class="row">
+      <div>
+
+      </div>
+      This is a button. BS is working
+    </div>
+  );
+}
+
+export default Button;
---
+++ src/components/ScrollBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './Button/Button';
+import './ScrollBar.css';
+
+function ScrollBar() {
+  return (
+    <div class="ScrollBar">
+      <Button name="Free Flow" active={No} />
+    </div>
+  );
+}
+
+export default ScrollBar;
